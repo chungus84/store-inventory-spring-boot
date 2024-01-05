cc87f8783858efb1ce421a338937e09925569a41
kenichi beveridge <kenbev84@gmail.com>
2024-01-05 20:11:36 +0000

    Added flash attribute if submittion failed

--- src/main/java/com/ltp/globalsuperstore/Constants.java
+++ src/main/java/com/ltp/globalsuperstore/Constants.java
@@ -5,4 +5,6 @@ public class Constants {
     public static final int NOT_FOUND = -1000;
 
     public static final String SUCCESS_STATUS = "success";
+
+    public static final String FAILED_STATUS = "failed";
 }
--- src/main/java/com/ltp/globalsuperstore/InventoryController.java
+++ src/main/java/com/ltp/globalsuperstore/InventoryController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Controller
 public class InventoryController {
@@ -32,12 +34,15 @@ public class InventoryController {
     @PostMapping("/submitItem")
     public String handleSubmit(Item item, RedirectAttributes redirectAttributes) {
         int index = getItemIndex(item.getId());
+        String status = Constants.SUCCESS_STATUS;
         if (index == Constants.NOT_FOUND) {
             items.add(item);
-        } else {
+        } else if (within5Days(item.getDate(), items.get(index).getDate())){
             items.set(index, item);
+        } else {
+            status = Constants.FAILED_STATUS;
         }
-        redirectAttributes.addFlashAttribute("status", Constants.SUCCESS_STATUS);
+        redirectAttributes.addFlashAttribute("status", status);
         return "redirect:/inventory";
 
     }
@@ -47,6 +52,11 @@ public class InventoryController {
             if (items.get(i).getId().equals(id)) return i;
         }
         return Constants.NOT_FOUND;
+    }
+
+    public boolean within5Days(Date newDate, Date oldDate) {
+        long diff = Math.abs(newDate.getTime() - oldDate.getTime());
+        return (int) (TimeUnit.MILLISECONDS.toDays(diff)) <= 5;
     }
 
 
